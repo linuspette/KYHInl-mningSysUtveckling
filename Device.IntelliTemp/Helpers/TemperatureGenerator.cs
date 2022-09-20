9e1ae180dc2ee49178161c84e3d7bd99d89cc3a9
linuspette <70110168+linuspette@users.noreply.github.com>
2022-09-20 13:02:03 +0200

    Written intellitemps algorithm. Still needs some fine-tuning.

--- Device.IntelliTemp/Helpers/TemperatureGenerator.cs
+++ Device.IntelliTemp/Helpers/TemperatureGenerator.cs
@@ -6,23 +6,56 @@ public static class TemperatureGenerator
 {
 
     private static Random random = new Random();
-    private static double minChange = -0.5;
-    private static double maxChange = 0.5;
+    private static double minChange = -0.25;
+    private static double maxChange = 0.25;
 
+    private static double minTemp = 16;
+    private static double maxTemp = 38;
+
+    private static double fanOnMultiplier = 1.35;
+    private static double fanOffMultiplier = 1.5;
+
+    private static bool validTemp = true;
+    public static bool fanIsOn = false;
 
     public static double TemperatureC { get; set; } = 20;
     public static double TemperatureF { get; set; } = ConvertToFahrenheit();
 
     public static void GenerateTemperature()
     {
-        var temp = (TemperatureC + (random.NextDouble() * (maxChange - (minChange)) + minChange));
+        double temp = 0;
+
+        do
+        {
+            temp = GetTemperature();
+            validTemp = ValidateTemperature(temp);
+        } while (!validTemp);
 
         TemperatureC = temp;
         TemperatureF = ConvertToFahrenheit();
     }
 
+    private static bool ValidateTemperature(double newTemp)
+    {
+        if (newTemp >= minTemp && newTemp <= maxTemp)
+            return true;
+
+        return false;
+    }
+
     private static double ConvertToFahrenheit()
     {
         return ((TemperatureC * (double)9/5) + 32);
+    }
+
+    private static double GetTemperature()
+    {
+        if (fanIsOn)
+        {
+            var temp = (random.NextDouble() * ((maxChange * fanOnMultiplier) - (minChange / fanOnMultiplier)) + minChange);
+            return (TemperatureC - (temp * fanOnMultiplier));
+        }
+
+        return (TemperatureC + (random.NextDouble() * ((maxChange * fanOffMultiplier) - minChange) + minChange));
     }
 }
--- Device.IntelliTemp/MainWindow.xaml.cs
+++ Device.IntelliTemp/MainWindow.xaml.cs
@@ -2,6 +2,8 @@
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
 using Device.IntelliTemp.Helpers;
 
 namespace Device.IntelliTemp
@@ -11,9 +13,18 @@ namespace Device.IntelliTemp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool timerIsOn = false;
+        private SolidColorBrush white = new SolidColorBrush(Colors.White);
+        private SolidColorBrush red = new SolidColorBrush(Colors.Red);
+        private DispatcherTimer timer = new DispatcherTimer();
+
+        public double UserSetWarningTemp { get; set; } = 30;
+
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += WarningOn;
             TemperatureGeneration().ConfigureAwait(false);
         }
 
@@ -22,8 +33,37 @@ namespace Device.IntelliTemp
             while (true)
             {
                 TemperatureGenerator.GenerateTemperature();
-                txtBlockTemperatureDisplay.Text = $"{Math.Round(TemperatureGenerator.TemperatureC, 1)}°C";
-                await Task.Delay(5000);
+                txtBlockTemperatureDisplay.Text = $"{Math.Round(TemperatureGenerator.TemperatureC, 0)}°C";
+                if (TemperatureGenerator.TemperatureC > UserSetWarningTemp && !timerIsOn)
+                {
+                    timerIsOn = true;
+                    timer.Start();
+                }
+                else if(TemperatureGenerator.TemperatureC < (UserSetWarningTemp-2))
+                {
+                    timer.Stop();
+                    timerIsOn = false;
+                }
+                await Task.Delay(500);
+            }
+        }
+
+        private void WarningOn(object? sender, EventArgs e)
+        {
+            txtBlockTemperatureDisplay.Foreground = txtBlockTemperatureDisplay.Foreground != red ? red : white;
+        }
+
+        private void BtnClick_FanSwitch(object sender, RoutedEventArgs e)
+        {
+            if (!TemperatureGenerator.fanIsOn)
+            {
+                btnFanControl.Content = "Shut off fan";
+                TemperatureGenerator.fanIsOn = true;
+            }            
+            else if (TemperatureGenerator.fanIsOn)
+            {
+                btnFanControl.Content = "Start fan";
+                TemperatureGenerator.fanIsOn = false;
             }
         }
     }
